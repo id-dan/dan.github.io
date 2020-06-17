a3b4d768c2a14cdfaa94493c1019c42f7348550e
Roman Danylyuk <roman.d89@gmail.com>
2020-06-17 09:58:11 +0300

    change script to override metatags

--- override-metadata.js
+++ override-metadata.js
@@ -1,6 +1,6 @@
-$(document).ready(function () {
-  console.log("ready");
-  $("head").append(
-    '<meta property="og:title" content="Override OG Title Metadata"/><meta property="og:type" content="website"/><meta property="og:image" content="http://www.party-news.de/party/images/termine/flyer/"/><meta property="og:description" content="Override OG Description Metadata""'
-  );
-});
+var link = document.createElement("meta");
+
+link.setAttribute("property", "og:title");
+link.content = "Override OG Title Metadata";
+
+document.getElementsByTagName("head")[0].appendChild(link);
